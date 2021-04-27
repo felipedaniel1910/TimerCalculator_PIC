2c46a80a94b4fa5c12eaca5e1347a33eeb15c193
FelipeDaniel <felipedaniel1910@hotmail.com>
2021-04-27 00:12:06 -0300

    Update main_psgui.py

--- main_psgui.py
+++ main_psgui.py
@@ -69,20 +69,3 @@ class Telapython:
 
 tela = Telapython()
 tela.iniciar()
-
-
-'''timer = float(input("Digite o tempo a ser contado: "))
-bits = int(input("Digite o numero de bits do contador: "))
-prescaler = int(input("Digite o prescaler usado: "))
-freq = float(input("Digite a frequencia do oscilador: "))
-           
-           
-carga = (timer-4*(1/freq)*prescaler*(2**bits))/-(4*(1/freq)*prescaler)
-
-carga = round(carga)
-carga = hex(carga)
-
-print("\nTMR0H = ",carga[2:4].upper()," e TMR0L = ",carga[4:6].upper())
-
-input("\nPressione qualquer tecla para sair...")
-'''
